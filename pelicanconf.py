ba2abc7647409cdb6c39c1bb28d90cf5919b3d36
Nantas Nardelli <nantas.nardelli@gmail.com>
2015-12-29 18:43:16 +0000

    Clean up config files

--- pelicanconf.py
+++ pelicanconf.py
@@ -5,34 +5,27 @@ from __future__ import unicode_literals
 AUTHOR = u'Nantas Nardelli'
 SITENAME = u'Nantas Nardelli'
 SITEURL = ''
-
 PATH = 'content'
-
 TIMEZONE = 'Europe/London'
-
 DEFAULT_LANG = u'en'
 
-# Feed generation is usually not desired when developing
+# Disable feed generation
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
-# Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+# Add subfolder to build
+STATIC_PATHS = ['images', 'extra/CNAME']
+EXTRA_PATH_METADATA = {'extra/CNAME': {'path': 'CNAME'}}
 
-# Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+# Blogroll / Social widget
+LINKS = ()
+SOCIAL = ()
 
+# Blog settings
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
-
-STATIC_PATHS = ['images', 'extra/CNAME']
-EXTRA_PATH_METADATA = {'extra/CNAME': {'path': 'CNAME'},}
